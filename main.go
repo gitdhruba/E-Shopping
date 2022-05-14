8bacf9eb70591a49f1547fe2ac7e1e77ab69aaf4
gitdhruba <sinhadhruba2001@gmail.com>
2022-05-14 18:20:38 +0530

    created basic server

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	//init router
+	r := mux.NewRouter()
+
+	books = append(books, Book{ID: "1", Isbn: "123456", Title: "Book1", Author: &Author{Firstname: "ABC", Lastname: "EFG"}})
+	books = append(books, Book{ID: "2", Isbn: "154894", Title: "Book2", Author: &Author{Firstname: "XYZ", Lastname: "EFG"}})
+	//route handlers
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
